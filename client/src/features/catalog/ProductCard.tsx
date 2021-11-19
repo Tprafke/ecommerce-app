08a1d16e803eda88d66bfaa3bd4dc34eace182e7
toby <rtprafke96@gmail.com>
2021-11-19 18:10:51 -0500

    Added shopping cart BasketSummary component

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -16,6 +16,7 @@ import agent from "../../app/api/agent";
 
 import { Product } from "../../app/models/product";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props {
   product: Product;
@@ -57,7 +58,7 @@ export default function ProductCard({ product }: Props) {
       />
       <CardContent>
         <Typography gutterBottom color='secondary' variant='h5' component='h5'>
-          ${(product.price / 100).toFixed(2)}
+          {currencyFormat(product.price)}
         </Typography>
         <Typography variant='body2' color='text.secondary'>
           {product.brand} / {product.type}
