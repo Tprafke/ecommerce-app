d19d1a6b8e0b06b2812bbe73b4bb09237ac3adea
toby <rtprafke96@gmail.com>
2022-09-11 19:27:29 -0400

    Refactoring state management - swapping context for redux toolkit

---
+++ client/src/app/store/configureStore.ts
@@ -0,0 +1,22 @@
+import { basketSlice } from './../../features/basket/basketSlice';
+import { useSelector } from 'react-redux';
+import { counterSlice } from './../../features/contact/counterSlice';
+import { configureStore } from "@reduxjs/toolkit"
+import { TypedUseSelectorHook, useDispatch } from 'react-redux';
+
+// export function configureStore() {
+//     return createStore(counterReducer);
+// }
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        basket: basketSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
