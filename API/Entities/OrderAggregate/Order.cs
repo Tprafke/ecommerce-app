7e7fefa0ddc9ccddf7c8b58dff1c177f2b804d0d
toby <rtprafke96@gmail.com>
2022-10-04 13:36:03 -0400

    Publish to Heroku

--- API/Entities/OrderAggregate/Order.cs
+++ API/Entities/OrderAggregate/Order.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,14 @@ namespace API.Entities.OrderAggregate
     {
         public int Id { get; set; }
         public string BuyerId { get; set; }
+        [Required]
         public ShippingAddress ShippingAddress { get; set; }
-        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
         public List<OrderItem> OrderItems { get; set; }
         public long Subtotal { get; set; }
         public long DeliveryFee { get; set; }
         public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+        [Required]
         public string PaymentIntentId { get; set; }
 
         public long GetTotal()
