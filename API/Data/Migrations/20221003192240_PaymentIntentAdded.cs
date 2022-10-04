89d75db713bb316ee95f5241b451c8682456d0df
toby <rtprafke96@gmail.com>
2022-10-03 20:58:04 -0400

    API and client - stripe integraton

---
+++ API/Data/Migrations/20221003192240_PaymentIntentAdded.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "43669f10-facc-4660-b769-bdf92ec3f5a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "63eb08d9-b3fc-4063-bf18-3b5aa0be6047");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "a6a4e4fe-43ff-43e5-9c33-276a8d2ff746");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d0fd948f-c695-4ef1-9b5b-b16e2bafc640");
+        }
+    }
+}
--- client/src/features/checkout/PaymentForm.tsx
+++ client/src/features/checkout/PaymentForm.tsx
@@ -1,57 +1,94 @@
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import { useFormContext } from 'react-hook-form';
-import AppTextInput from '../../app/components/AppTextInput';
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import { useFormContext } from "react-hook-form";
+import AppTextInput from "../../app/components/AppTextInput";
+import {
+  CardCvcElement,
+  CardExpiryElement,
+  CardNumberElement,
+} from "@stripe/react-stripe-js";
+import { StripeInput } from "./StripeInput";
+import { StripeElementType } from "@stripe/stripe-js";
 
-export default function PaymentForm() {
-  const {control} = useFormContext();
+interface Props {
+  cardState: {
+    elementError: { [key in StripeElementType]?: string };
+  };
+  onCardInputChange: (event: any) => void;
+}
+
+export default function PaymentForm({cardState, onCardInputChange}: Props) {
+  const { control } = useFormContext();
+
   return (
     <>
-      <Typography variant="h6" gutterBottom>
+      <Typography variant='h6' gutterBottom>
         Payment method
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <AppTextInput name="nameOnCard" label="Name on Card" control={control}/>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            //required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
+          <AppTextInput
+            name='nameOnCard'
+            label='Name on Card'
+            control={control}
           />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
-            //required
-            id="expDate"
-            label="Expiry date"
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardNumber}
+            helperText={cardState.elementError.cardNumber}
+            id='cardNumber'
+            label='Card number'
             fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
+            autoComplete='cc-number'
+            variant='outlined'
+            InputLabelProps={{ shrink: true }}
+            InputProps={{
+              inputComponent: StripeInput,
+              inputProps: {
+                component: CardNumberElement,
+              },
+            }}
           />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
-            //required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardExpiry}
+            helperText={cardState.elementError.cardExpiry}
+            id='expDate'
+            label='Expiry date'
             fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
+            autoComplete='cc-exp'
+            variant='outlined'
+            InputLabelProps={{ shrink: true }}
+            InputProps={{
+              inputComponent: StripeInput,
+              inputProps: {
+                component: CardExpiryElement,
+              },
+            }}
           />
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
+        <Grid item xs={12} md={6}>
+          <TextField
+            onChange={onCardInputChange}
+            error={!!cardState.elementError.cardCvc}
+            helperText={cardState.elementError.cardCvc}
+            id='cvv'
+            label='CVV'
+            fullWidth
+            autoComplete='cc-csc'
+            variant='outlined'
+            InputLabelProps={{ shrink: true }}
+            InputProps={{
+              inputComponent: StripeInput,
+              inputProps: {
+                component: CardCvcElement,
+              },
+            }}
           />
         </Grid>
       </Grid>
